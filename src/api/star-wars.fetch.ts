59c040c307efe6b97a65bab4012924aeeaad68cd
RajabovIlyas <ilyasrajabow@gmail.com>
2025-02-09 21:43:49 +0500

    feat: changed settings

--- src/api/star-wars.fetch.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import { StarWarsResponse } from '../app/types';
-import { API_URL } from './constants.ts';
-
-export const fetchStarWars = async (searchQuery: string) => {
-    return axios.get<StarWarsResponse>(API_URL, {
-        params: { search: searchQuery },
-    });
-};
