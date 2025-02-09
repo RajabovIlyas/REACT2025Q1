c31a643ffc25a4781f159265587bb9e3deeab00d
RajabovIlyas <ilyasrajabow@gmail.com>
2025-02-09 21:42:57 +0500

    feat: added routing

---
+++ src/app/app.tsx
@@ -0,0 +1,18 @@
+import './styles/styles.css';
+import Home from '../pages/home/home.tsx';
+import ErrorBoundary from '../shared/error-boundary.tsx';
+import { Route, Routes } from 'react-router';
+import Page404 from '../pages/page-404/page-404.tsx';
+
+const App = () => {
+    return (
+        <ErrorBoundary>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="*" element={<Page404 />} />
+            </Routes>
+        </ErrorBoundary>
+    );
+};
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './app/App.tsx';
-import './app/styles/styles.css';
+import App from './app/app.tsx';
+import { BrowserRouter } from 'react-router';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </StrictMode>,
 );
